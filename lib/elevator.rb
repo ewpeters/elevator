b7a556308c9b57c928f60b3d078a945d63aa6b2f
Erik Petersen <erik@telmate.com>
2013-05-29 12:59:32 -0700

    added up and down buttons for each floor

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -6,12 +6,19 @@ class Elevator
     (0..@number_of_floors).each do |floor_number|
       (class << self; self; end).class_eval do
         define_method("floor_#{floor_number}".to_sym) { go_to_floor(floor_number) }
+        define_method("floor_#{floor_number}_up".to_sym) { call_elevator(floor_number) }
+        define_method("floor_#{floor_number}_down".to_sym) { call_elevator(floor_number) }
       end
     end
   end
 
   private
   def go_to_floor(floor)
+    @current_floor = floor
+    return @current_floor
+  end
+
+  def call_elevator(floor)
     @current_floor = floor
     return @current_floor
   end
--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -17,4 +17,38 @@ describe Elevator do
 
     elevator.current_floor.should == 20
   end
+
+  it "should go to floor x when floor_x_up is called" do
+    elevator = Elevator.new(100)
+    elevator.floor_20_up
+
+    elevator.current_floor.should == 20
+  end
+
+  it "should go to floor x when floor_x_down is called" do
+    elevator = Elevator.new(100)
+    elevator.floor_20_down
+
+    elevator.current_floor.should == 20
+  end
+
+  it "should have an up button for each floor" do
+    num_floors = rand(100)
+
+    elevator = Elevator.new(num_floors)
+
+    (0..num_floors).each do |number|
+      elevator.should respond_to("floor_#{number}_up".to_sym)
+    end
+  end
+
+  it "should have a down button for each floor" do
+    num_floors = rand(100)
+
+    elevator = Elevator.new(num_floors)
+
+    (0..num_floors).each do |number|
+      elevator.should respond_to("floor_#{number}_down".to_sym)
+    end
+  end
 end
